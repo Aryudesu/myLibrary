7111343d36b906c3ad30a7bb70a645ed3f84df02
Aryudesu <suwako_a-u-@hotmail.co.jp>
2025-03-15 02:14:15 +0900

    転倒数

--- math/inversion_count.py
+++ math/inversion_count.py
@@ -1,38 +1,36 @@
-class BIT:
-    def __init__(self, n):
-        """要素数nで初期化する"""
-        self.n = n
-        self.tree = [0] * (n + 1)
-
-    def add(self, idx, val):
-        """idxにvalを加算する"""
-        while idx <= self.n:
-            self.tree[idx] += val
-            idx += idx & -idx
-
-    def sum(self, idx):
-        """idxまでの累積和を求める"""
-        res = 0
-        while idx > 0:
-            res += self.tree[idx]
-            idx -= idx & -idx
-        return res
-
-    def range_sum(self, left, right):
-        """区間[left, right]の和を求める"""
-        return self.sum(right) - self.sum(left - 1)
+from atcoder.fenwicktree import FenwickTree
+from sortedcontainers import SortedSet
 
 
-def inversion_count(arr):
-    """
-    転倒数を計算する
-    """
-    sorted_arr = sorted(set(arr))
-    compress = {val: i + 1 for i, val in enumerate(sorted_arr)}
-    bit = BIT(len(sorted_arr))
-    inv_count = 0
-    for i in reversed(range(len(arr))):
-        inv_count += bit.sum(compress[arr[i]] - 1)
-        bit.add(compress[arr[i]], 1)
+def inversion_count(arr: list) -> int:
+    result = 0
+    ft = FenwickTree(max(arr) + 1)
+    for i, a in enumerate(A):
+        result += i - ft.sum(0, a + 1)
+        ft.add(a, 1)
+    return result
 
-    return inv_count
+
+N, M = [int(l) for l in input().split()]
+A = [int(l) for l in input().split()]
+origin = inversion_count(A)
+
+data = SortedSet()
+for idx in range(N):
+    a = A[idx]
+    tmp = M - A[idx]
+    data.add((tmp, idx))
+result = []
+for m in range(M):
+    while True:
+        if len(data) == 0:
+            break
+        num, idx = data.pop(0)
+        if num > m:
+            data.add((num, idx))
+            break
+        else:
+            origin = origin - (N - idx - 1) + idx
+    result.append(origin)
+for r in result:
+    print(r)
