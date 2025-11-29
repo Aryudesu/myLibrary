d3d3f93e4ed81d4f279345313144b8c5196d9228
Aryu <suwako_a-u-@hotmail.co.jp>
2025-11-29 19:00:30 +0900

    BestKthSumの修正

--- data/bestKthSum.py
+++ data/bestKthSum.py
@@ -5,40 +5,70 @@ class BestKthSum:
     上位K番目の値の総和を計算します．
     値の更新がされることを考慮．
     """
-    def __init__(self, K: int, values: list[int] = [0]):
+
+    def __init__(self, K: int, values: list[int]):
+        assert 0 <= K <= len(values)
         self.k = K
-        self.data = list(values)
-        self.n = len(self.data)
-        self.sorted = SortedList(-x for x in self.data)
+        self.data = values[:]
+        self.n = len(values)
+
+        self.large = SortedList()
+        self.small = SortedList()
         self.sum = 0
-        for k in range(K):
-            self.sum += self.sorted[k]
-    
-    def update(self, idx: int, new_val: int)-> int:
+
+        for i, v in enumerate(self.data):
+            self.small.add((v, i))
+
+        for _ in range(self.k):
+            if not self.small:
+                break
+            v, i = self.small.pop()
+            self.large.add((v, i))
+            self.sum += v
+
+    def _rebalance(self):
         K = self.k
-        sortedData = self.sorted
 
-        # 変数設定
-        kth = -sortedData[K-1]
-        prev = self.data[idx]
-        now = new_val
-        self.data[idx] = now
-        # ソート結果の更新
-        sortedData.discard(-prev)
-        sortedData.add(-now)
-        # 上位K番目の値を取得
-        next_kth = -sortedData[K - 1]
+        while len(self.large) > K:
+            v, i = self.large.pop(0)
+            self.small.add((v, i))
+            self.sum -= v
 
-        # 入る値
-        self.sum += max(next_kth, now)
+        while len(self.large) < K and self.small:
+            v, i = self.small.pop()
+            self.large.add((v, i))
+            self.sum += v
 
-        # 外れる値
-        if prev > next_kth:
-            self.sum -= prev
+        while self.large and self.small and self.large[0][0] < self.small[-1][0]:
+            v_small, i_small = self.large.pop(0)
+            v_big,   i_big   = self.small.pop()
+
+            self.large.add((v_big, i_big))
+            self.small.add((v_small, i_small))
+
+            self.sum += v_big - v_small
+
+    def update(self, idx: int, new_val: int) -> int:
+        old_val = self.data[idx]
+        if old_val == new_val:
+            return self.sum
+
+        self.data[idx] = new_val
+        old_pair = (old_val, idx)
+
+        if old_pair in self.large:
+            self.large.remove(old_pair)
+            self.sum -= old_val
         else:
-            self.sum -= kth
+            self.small.remove(old_pair)
 
+        new_pair = (new_val, idx)
+        self.large.add(new_pair)
+        self.sum += new_val
+
+        self._rebalance()
         return self.sum
+
 
 
 # === ABC306 E
