7d0f45fdd6c25f7cc34dca4b37826bb8acce6c99
Aryu <suwako_a-u-@hotmail.co.jp>
2026-02-07 00:49:11 +0900

    ランレングスクラス最適化

--- datastruct/run_length.py
+++ datastruct/run_length.py
@@ -1,70 +1,74 @@
+from collections import deque
+from typing import Any, Tuple
+
 class RunLength:
     """
     ランレングス符号クラス
     Edited by Aryu
     """
     def __init__(self) -> None:
-        self.data = []
-        self.pointer = 0
+        self.data: deque[Tuple[Any, int]] = deque()
+        self.size = 0
 
-    def insert(self, x, n):
+    def appendRight(self, x: Any, n: int)->None:
         """xをa個追加"""
-        if len(self.data):
-            if self.data[-1][0] == x:
-                self.data[-1][1] += n
-            else:
-                self.data.append([x, n])
+        if self.data and self.data[-1][0] == x:
+            v, c = self.data[-1]
+            self.data[-1] = (v, c + n)
         else:
-            self.data.append([x, n])
+            self.data.append((x, n))
+        self.size += n
+    
+    def appendLeft(self, x: Any, n: int)->None:
+        if self.data and self.data[0][0] == x:
+            v, c = self.data[0]
+            self.data[0] = (v, c + n)
+        else:
+            self.data.appendleft((x, n))
+        self.size += n
 
-    def popData(self, n):
-        """末尾からn個取得"""
-        tmpN = n
-        result = []
-        while True:
-            if self.data[-1][1] > tmpN:
-                self.data[-1][1] -= tmpN
-                result.append([self.data[-1][0], tmpN])
-                return result
-            elif self.data[-1][1] == tmpN:
-                result.append([self.data[-1][0], tmpN])
-                self.data.pop()
-                return result
+    def popRight(self, n: int)->list[Tuple[Any, int]]:
+        """末尾からn個取得．個数が不足している場合は全て取得．"""
+        if n == 0 or self.size == 0:
+            return []
+        num = min(n, self.size)
+        result: list[Tuple[Any, int]] = []
+        self.size -= num
+        while num > 0:
+            v, c = self.data[-1]
+            if c > num:
+                self.data[-1] = (v, c - num)
+                result.append((v, num))
             else:
-                tmpN -= self.data[-1][1]
-                result.append([self.data[-1][0], self.data[-1][1]])
-                self.data.pop()
+                result.append(self.data.pop())
+            num -= c
         return result
 
-    def deque(self, n):
+    def popLeft(self, n: int)->list[Tuple[Any, int]]:
         """先頭からデータをn個取得"""
-        tmpN = n
-        result = []
-        while True:
-            pointer = self.pointer
-            if len(self.data) <= pointer:
-                break
-            if self.data[pointer][1] > tmpN:
-                self.data[pointer][1] -= tmpN
-                result.append([self.data[pointer][0], tmpN])
-                return result
-            elif self.data[pointer][1] == tmpN:
-                result.append([self.data[pointer][0], self.data[pointer][1]])
-                self.pointer += 1
-                return result
+        if n == 0 or self.size == 0:
+            return []
+        num = min(n, self.size)
+        result: list[Tuple[Any, int]] = []
+        self.size -= num
+        while num > 0:
+            v, c = self.data[0]
+            if c > num:
+                self.data[0] = (v, c - num)
+                result.append((v, num))
             else:
-                tmpN -= self.data[pointer][1]
-                result.append([self.data[pointer][0], self.data[pointer][1]])
-                self.pointer += 1
+                result.append(self.data.popleft())
+            num -= c
         return result
 
-    def show(self):
-        """デバッグ用表示"""
-        print(self.data)
-        print(self.pointer)
+    def __bool__(self):
+        return len(self.data) > 0
+
+    def __len__(self):
+        return self.size
 
 
-# *** Example (ABC347 D) ***
+# *** Example (ABC413 C) ***
 rl = RunLength()
 result = []
 Q = int(input())
@@ -72,11 +76,11 @@ for _ in range(Q):
     res = 0
     t, *p = [int(l) for l in input().split()]
     if t == 1:
-        x, c = p
-        rl.insert(x, c)
+        c, x = p
+        rl.appendRight(x, c)
     elif t == 2:
-        c = p[0]
-        dat = rl.deque(c)
+        k = p[0]
+        dat = rl.popLeft(k)
         for d in dat:
             res += d[0] * d[1]
         result.append(res)
