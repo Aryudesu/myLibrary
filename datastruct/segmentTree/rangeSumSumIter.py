f87e79b94df6f62b345a30ab9b7236891da186d5
Aryu <suwako_a-u-@hotmail.co.jp>
2026-02-26 22:35:12 +0900

    セグ木追加

--- datastruct/segmentTree/rangeSumSumIter.py
+++ datastruct/segmentTree/rangeSumSumIter.py
@@ -2,11 +2,12 @@ import sys
 sys.setrecursionlimit(1_000_000)
 input = sys.stdin.readline
 
-class RangeAssignSumIter:
+
+class RangeAddSumIter:
     """
-    任意値の区間代入 + 区間和セグ木
+    区間加算 + 区間和セグ木
     """
-    __slots__ = ("n", "size", "log", "seg", "lazy_flag", "lazy_val", "length")
+    __slots__ = ("n", "size", "log", "seg", "lazy", "length")
 
     def __init__(self, n: int, init: list[int] | None = None) -> None:
         self.n = n
@@ -18,8 +19,7 @@ class RangeAssignSumIter:
         self.size = size
         self.log = log
         self.seg = [0] * (2 * size)
-        self.lazy_flag = [False] * (2 * size)
-        self.lazy_val = [0] * (2 * size)
+        self.lazy = [0] * (2 * size)
         self.length = [0] * (2 * size)
 
         for k in range(size, 2 * size):
@@ -37,49 +37,44 @@ class RangeAssignSumIter:
                 seg[k] = seg[k * 2] + seg[k * 2 + 1]
 
     def _apply(self, k: int, v: int) -> None:
-        """ノード k に「区間全部 v」の代入を適用"""
-        self.seg[k] = v * self.length[k]
-        self.lazy_flag[k] = True
-        self.lazy_val[k] = v
+        """ノード k に「区間全体に +v」の加算を適用"""
+        self.seg[k] += v * self.length[k]
+        self.lazy[k] += v
 
     def _push_to_leaf(self, k: int) -> None:
-        """根からノード k までの lazy をすべて子に落とす"""
+        """根からノード k までに溜まっている遅延をすべて子へ落とす"""
         seg = self.seg
-        lazy_flag = self.lazy_flag
-        lazy_val = self.lazy_val
+        lazy = self.lazy
         length = self.length
         for h in range(self.log, 0, -1):
             p = k >> h
-            if lazy_flag[p]:
-                v = lazy_val[p]
+            if lazy[p] != 0:
+                v = lazy[p]
                 c1 = p << 1
                 c2 = c1 | 1
-                seg[c1] = v * length[c1]
-                seg[c2] = v * length[c2]
-                lazy_flag[c1] = True
-                lazy_flag[c2] = True
-                lazy_val[c1] = v
-                lazy_val[c2] = v
-                lazy_flag[p] = False
+                seg[c1] += v * length[c1]
+                seg[c2] += v * length[c2]
+                lazy[c1] += v
+                lazy[c2] += v
+                lazy[p] = 0
 
     def _recalc_from_leaf(self, k: int) -> None:
-        """ノード k から根まで seg を再計算"""
+        """ノード k から根まで seg を再計算（lazy が 0 のところだけ）"""
         seg = self.seg
-        lazy_flag = self.lazy_flag
+        lazy = self.lazy
         k >>= 1
         while k:
-            if not lazy_flag[k]:
+            if lazy[k] == 0:
                 seg[k] = seg[k * 2] + seg[k * 2 + 1]
             k >>= 1
 
-    def assign(self, l: int, r: int, v: int) -> None:
-        """A[l:r] を v で埋める"""
+    def add(self, l: int, r: int, v: int) -> None:
+        """A[l:r] に v を加算"""
         if l >= r:
             return
         size = self.size
         seg = self.seg
-        lazy_flag = self.lazy_flag
-        lazy_val = self.lazy_val
+        lazy = self.lazy
         length = self.length
 
         l += size
@@ -91,15 +86,13 @@ class RangeAssignSumIter:
 
         while l < r:
             if l & 1:
-                seg[l] = v * length[l]
-                lazy_flag[l] = True
-                lazy_val[l] = v
+                seg[l] += v * length[l]
+                lazy[l] += v
                 l += 1
             if r & 1:
                 r -= 1
-                seg[r] = v * length[r]
-                lazy_flag[r] = True
-                lazy_val[r] = v
+                seg[r] += v * length[r]
+                lazy[r] += v
             l >>= 1
             r >>= 1
 
@@ -107,7 +100,7 @@ class RangeAssignSumIter:
         self._recalc_from_leaf(r0 - 1)
 
     def sum(self, l: int, r: int) -> int:
-        """A[l:r] の総和"""
+        """A[l:r] の総和を計算します"""
         if l >= r:
             return 0
         size = self.size
@@ -131,14 +124,31 @@ class RangeAssignSumIter:
             r >>= 1
         return res
 
-    def point_set(self, i: int, v: int) -> None:
-        """A[i] = v"""
-        size = self.size
-        k = i + size
-        self._push_to_leaf(k)
-        self._apply(k, v)
-        self._recalc_from_leaf(k)
+    def point_add(self, i: int, v: int) -> None:
+        """A[i] に v を加算します"""
+        self.add(i, i + 1, v)
 
     def get(self, i: int) -> int:
-        """A[i] を返す"""
+        """A[i] を返却します"""
         return self.sum(i, i + 1)
+
+
+# === AWC0013E
+
+N, Q = map(int, input().split())
+C = list(map(int, input().split()))
+st = RangeAddSumIter(N, C)
+result = []
+for _ in range(Q):
+    n, *query = list(map(int, input().split()))
+    if n == 1:
+        l, r, v = query
+        st.add(l-1, r, v)
+    elif n == 2:
+        l, r = query
+        result.append(st.sum(l-1, r))
+    else:
+        raise ValueError()
+for r in result:
+    print(r)
+
