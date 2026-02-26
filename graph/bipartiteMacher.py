62713583bd9977b1a5d7bef21ed482420d10a61a
Aryu <suwako_a-u-@hotmail.co.jp>
2026-02-26 19:08:46 +0900

    最大マッチング問題

--- graph/bipartiteMacher.py
+++ graph/bipartiteMacher.py
@@ -1,31 +1,35 @@
-class BipartiteMatcher:
-    """左側: 0..n_left-1, 右側: 0..n_right-1 の二部グラフ"""
-    def __init__(self, n_left: int, n_right: int):
-        self.n_left = n_left
-        self.n_right = n_right
-        self.g = [[] for _ in range(n_left)]
-        self.matchR = [-1] * n_right  # 右側 → 対応する左側
+from atcoder.maxflow import MFGraph
 
-    def add_edge(self, l: int, r: int):
-        """左 l と 右 r の間に辺を張る"""
-        self.g[l].append(r)
+class FlowBipartiteMatching:
+    """左側 N点, 右側 M点 の0-indexed二部グラフの最大マッチング用クラス"""
+    def __init__(self, N: int, M: int):
+        self.N = N
+        self.M = M
+        self.MBase = N
+        self.mf = MFGraph(N + M + 2)
+        self.s = N + M
+        self.t = N + M + 1
+        for n in range(N):
+            self.mf.add_edge(self.s, n, 1)
+        for m in range(M):
+            self.mf.add_edge(self.MBase + m, self.t, 1)
 
-    def max_matching(self) -> int:
-        """最大マッチングのサイズを返す"""
-        result = 0
-        def dfs(v: int, memo: list[bool]) -> bool:
-            for u in self.g[v]:
-                if memo[u]:
-                    continue
-                memo[u] = True
-                if self.matchR[u] == -1 or dfs(self.matchR[u], memo):
-                    self.matchR[u] = v
-                    return True
-            return False
-        for v in range(self.n_left):
-            memo = [False] * self.n_right
-            if dfs(v, memo):
-                result += 1
-        return result
+    def addEdge(self, src: int, dst: int)->None:
+        """左側の点から右側の点に辺を張ります"""
+        assert 0 <= src < self.N
+        assert 0 <= dst < self.M
+        self.mf.add_edge(src, self.MBase + dst, 1)
+    
+    def maxMatching(self)->int:
+        """最大マッチングの結果を計算します"""
+        return self.mf.flow(self.s, self.t)
 
+# === AWC0013E
 
+N, M = map(int, input().split())
+fm = FlowBipartiteMatching(N, M )
+for n in range(N):
+    K, *C = list(map(int, input().split()))
+    for c in C:
+        fm.addEdge(n, c-1)
+print(fm.maxMatching())
